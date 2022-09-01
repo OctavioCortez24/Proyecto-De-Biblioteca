92e84709310842f94ee42b4f55b6a1aff26755c1
OctavioCortez24 <oc31549@gmail.com>
2022-09-01 13:52:59 -0300

    Termine de implementar la capa de persistencia

--- Biblioteca/Modelo.java
+++ Biblioteca/Modelo.java
@@ -3,28 +3,6 @@ package Biblioteca;
 import java.util.ArrayList;
 
 public class Modelo {
-    public static void CargarArrays(ArrayList<Socio> Socios, ArrayList<Libro> Libros, ArrayList<Pedido> Pedidos) {
-        //Cargo el array Socio-------------
-        ArrayList<String> SociosCargados = GestorArchivos.cargarArray("ArraySocios.txt");
-
-        for (int i = 0; i < SociosCargados.size(); i++) {
-            Socio socio = Controlador.recuperarSocio(SociosCargados.get(i));
-            Socios.add(socio);
-        }
-        //-----------------------------
-
-        ArrayList<String> LibrosCargados = GestorArchivos.cargarArray("ArrayLibros.txt");
-        for (int i = 0; i < LibrosCargados.size(); i++) {
-            Libro libro = Controlador.recuperarLibro(LibrosCargados.get(i));
-            Libros.add(libro);
-        }
-        ArrayList<String> PedidosCargados = GestorArchivos.cargarArray("ArrayPedidos.txt");
-        for (int i = 0; i < PedidosCargados.size(); i++) {
-            Pedido pedido = Controlador.recuperarPedido(PedidosCargados.get(i));
-            Pedidos.add(pedido);
-        }
-    }
-
     public static void GuardarPedidos(ArrayList<Pedido> Pedidos) {
         /*Debo copiar los datos de mis arrays de pedido porque el metodo de gestor de archivos, guardar ayrray,
         solamente permite array de tipo String.
@@ -54,5 +32,37 @@ public class Modelo {
         ArrayList<String> LibrosString = Controlador.CopiarDatosLibros(Libros);
         GestorArchivos.guardarArray(LibrosString, "ArrayLibros.txt");
     }
+
+    public static void CargarArrays(ArrayList<Socio> Socios, ArrayList<Libro> Libros, ArrayList<Pedido> Pedidos) {
+        /* Ahora recupero los arrays de String y los vuelvo a convertir en array de objetos.
+         */
+        //Cargo el array Socio-------------
+        ArrayList<String> SociosCargados = GestorArchivos.cargarArray("ArraySocios.txt");
+
+        for (int i = 0; i < SociosCargados.size(); i++) {
+
+            Socio socio = Controlador.recuperarSocio(SociosCargados.get(i));
+            Socios.add(socio);
+        }
+        //-----------------------------
+
+        //Cargo el array de Libros
+        ArrayList<String> LibrosCargados = GestorArchivos.cargarArray("ArrayLibros.txt");
+        for (int i = 0; i < LibrosCargados.size(); i++) {
+            Libro libro = Controlador.recuperarLibro(LibrosCargados.get(i));
+            Libros.add(libro);
+        }
+        //-----------------------------
+
+        //Cargo de array de Pedidos
+        ArrayList<String> PedidosCargados = GestorArchivos.cargarArray("ArrayPedidos.txt");
+        for (int i = 0; i < PedidosCargados.size(); i++) {
+            Pedido pedido = Controlador.recuperarPedido(PedidosCargados.get(i));
+            Pedidos.add(pedido);
+        }
+        //-----------------------------
+    }
+
+
 
 }
